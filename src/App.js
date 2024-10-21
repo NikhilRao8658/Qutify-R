73859fb5360749ec8ee0bcd0b3c34991267df674
unknown <nrao8658@gmail.com>
2024-10-21 15:15:04 +0530

    qtfy

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Details from './Components/Details';
+import FirstPage from './Components/FirstPage';
+import FeedbackPage from './Pages/Feedback';
+import Error from './Pages/Error';
+import Login from './Pages/Login';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<FirstPage />} />
+        <Route path='/Details' element={<Details />} />
+        <Route path='/feedback' element={<FeedbackPage/>}/>
+        <Route path='/Login' element={<Login/>}/>
+
+        <Route path='*' element={<Error/>}/>
+
+      </Routes>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
